7b42f5eb5b6f14ed600079b37c8e4b8ce12d987c
Arezoo Dahesh <arezoo.dahesh@gmail.com>
2023-03-11 21:17:21 +0330

    Define take_screenshot function

--- dino.py
+++ dino.py
@@ -1,7 +1,30 @@
 # ------ Import libraries ------
 import webbrowser
 import pyautogui
+from PIL import Image, ImageGrab 
+import cv2
 import time
+import numpy as np
+# importmatplotlib.pyplot as plt
+
+# ------ Get current mouse position ------
+# print(pyautogui.position())
+
+# top-left
+# Point(x=553, y=349)
+
+# bottom-right
+# Point(x=1311, y=443)
+
+               
+# ------ Define functions ------
+def take_screenshot():
+    image = ImageGrab.grab(bbox =(553, 349, 1311, 443)).convert('L')
+    img = np.array(image)
+    # binarize image
+    _, thresh_image = cv2.threshold(img,210,255,cv2.THRESH_BINARY)
+    # plt.imshow(thresh_image)
+    return thresh_image
 
 
 # ------ Open URL ------
@@ -18,4 +41,4 @@ webbrowser.open('http://chromedino.com/')
 time.sleep(5)
 for i in range(1,500):
     pyautogui.press('space')
-           
+
